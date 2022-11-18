c8201011291ce0df0f65c9c3d98f6d8348d7c531
LeonAngelit <81989897+LeonAngelit@users.noreply.github.com>
2022-11-18 09:22:11 +0100

    dependencies and dbContext

---
+++ DotnetFinalAssessment/Data/dbContext.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore;
+using DotnetFinalAssessment.Models;
+
+namespace DotnetFinalAssessment.Data
+{
+    public class dbContext: DbContext
+    {
+        public DbSet<Question> Questions { get; set; }
+        public DbSet<User> Users { get; set; }
+
+        public dbContext(DbContextOptions<dbContext> options) : base(options) { }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+
+
+
+            modelBuilder.Entity<Question>(question =>
+            {
+                question.ToTable("questions");
+                question.HasKey(p => p.Id);
+                question.Property(p => p.Title).IsRequired().HasMaxLength(150);
+                question.Property(p => p.Subject).IsRequired();
+                question.Property(p => p.Options).IsRequired();
+
+            });
+
+
+
+            modelBuilder.Entity<User>(user =>
+            {
+                user.ToTable("users");
+                user.HasKey(p => p.Id);
+                user.Property(p => p.email).IsRequired().HasMaxLength(60);
+                user.Property(p => p.answers).IsRequired();
+
+            });
+        }
+    }
+}
