eecd2ca1f902a89b1134a13d357f4a39110e75f2
LeonAngelit <81989897+LeonAngelit@users.noreply.github.com>
2022-11-18 09:08:54 +0100

    project folder

---
+++ DotnetFinalAssessment/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace DotnetFinalAssessment.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
